ff3935b911925fdea86e100f51f10b7c55ddd027
rajneessh_yadav <yadavrajneesh481@gmail.com>
2023-12-26 00:19:25 +0530

    multiply done

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ app.get("/", (req, res) => {
   });
 });
 
+app.get("/multiply/:num1/:num2", (req, res) => {
+  const num1 = req.params.num1;
+  const n1 = parseInt(num1);
+  const num2 = req.params.num2;
+  const n2 = parseInt(num2);
+  res.status(200).json({
+    multiply: n1 * n2,
+  });
+});
 app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
 });
